01720c7539a8475a49326964c09b2e160b1df772
wangkangandroid <491834726@qq.com>
2020-05-26 17:19:54 +0800

    提交文件

---
+++ app/src/main/java/cc/huanmei/httpwww/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package cc.huanmei.httpwww.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cc.huanmei.httpwww.appbase.AppMainActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        startActivity(Intent(this, AppMainActivity::class.java))
+    }
+}
