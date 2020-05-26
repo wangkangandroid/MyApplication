aad93a7df6e20fc48a75e406a1e4b3c9cc006aa6
wangkangandroid <491834726@qq.com>
2020-05-26 17:51:34 +0800

    测试AS提交

--- app/src/main/java/cc/huanmei/httpwww/myapplication/MainActivity.kt
+++ app/src/main/java/cc/huanmei/httpwww/myapplication/MainActivity.kt
@@ -1,16 +1,12 @@
 package cc.huanmei.httpwww.myapplication
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import cc.huanmei.httpwww.appbase.AppMainActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ches
-        startActivity(Intent(this, AppMainActivity::class.java))
     }
 }
